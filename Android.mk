a76693d27161569bdf56e4e073ac062ac3f938c5
dread <dreadcreator5@gmail.com>
2024-01-24 12:29:34 +0530

    rosemary: Ship firmware from V14.0.8.0 TKLMIXM
    Signed-off-by: dread <dreadcreator5@gmail.com>

--- Android.mk
+++ Android.mk
@@ -6,4 +6,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),rosemary)
 
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,0407eb8b704ec979ee57ecb539c2d2677ee34f02)
+$(call add-radio-file-sha1-checked,radio/cam_vpu1.img,4b0842255df77c78e37369df1b4d9b5291ffe183)
+$(call add-radio-file-sha1-checked,radio/cam_vpu2.img,0597159e8cadf2f85f4e5528129405c8875010d6)
+$(call add-radio-file-sha1-checked,radio/cam_vpu3.img,458e9ac71b79548611b2c1a982b65f1193503222)
+$(call add-radio-file-sha1-checked,radio/gz.img,30040d023c87547ff7a4330d38b3e63be3d94a3f)
+$(call add-radio-file-sha1-checked,radio/lk.img,a18c2c361a86bffa228a9a684b8cb3af598353f9)
+$(call add-radio-file-sha1-checked,radio/md1img.img,f4b562e4d36863a1cec694fd509285e763fd613e)
+$(call add-radio-file-sha1-checked,radio/scp.img,46e2acc3f75764a14ae392d7ac5ce40f4bf74dc5)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,7ab1169f401d4b8df6ee2c72a3034ed5da163904)
+$(call add-radio-file-sha1-checked,radio/sspm.img,25f3b6548758e3dbe08c5709d49535cb02de5c91)
+$(call add-radio-file-sha1-checked,radio/tee.img,2531be4f2d1ce23482e94be0b238c59650e7dae4)
+
 endif
